68365b296ccfe85fbecf1b6c9bea5cda701c2b56
mario <mario.florez249@gmail.com>
2024-07-07 18:06:30 -0500

    Clase 10: Variables en Java

---
+++ HolaMundo/src/Variables.java
@@ -0,0 +1,18 @@
+
+public class Variables {
+	
+	public static void main(String[] args) {
+		//Declarando una variable
+		int speed;
+		
+		//Asignamos el valor a una variable
+		speed = 10;
+		System.out.println(speed);
+		
+		//Declaración y asignación en la misma línea
+		int salary = 1000;
+		String employeeName = "Mario";
+		System.out.println(employeeName);
+	}
+
+}
