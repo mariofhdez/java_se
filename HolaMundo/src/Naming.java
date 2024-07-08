75598d636c62b5f359eed5803714ebe53ec5b20a
mario <mario.florez249@gmail.com>
2024-07-07 19:34:23 -0500

    Clase 12: Convención de nombres en Java

---
+++ HolaMundo/src/Naming.java
@@ -0,0 +1,26 @@
+
+public class Naming {
+	
+	public static void main(String[] args) {
+		int celphone = 33337777;
+		int celPhone = 55553333;
+		System.out.println(celphone);
+		System.out.println(celPhone);
+		
+		String $countryName = "Colombia";
+		String _backgroundColor = "Green";
+		
+		String currency$col = "COP";
+		String background_color = "Gray";
+		
+		String currency_mex = "MXN";
+		String backgroundColor_ = "White";
+		
+		// Constantes
+		
+		int POSITION = -5;
+		int MAX_WIDTH = 9999;
+		int MIN_WIDTH = 1;
+	}
+
+}
