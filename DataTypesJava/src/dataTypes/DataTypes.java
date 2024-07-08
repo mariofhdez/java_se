0020fa8ac40b0039f8bde2b5fd9dfce95d37888e
mario <mario.florez249@gmail.com>
2024-07-07 20:10:03 -0500

    Clase 14: Tipos de datos numéricos

---
+++ DataTypesJava/src/dataTypes/DataTypes.java
@@ -0,0 +1,16 @@
+package dataTypes;
+
+public class DataTypes {
+	
+	public static void main(String[] args) {
+		int n = 1234567890; //cantidad máxima de digitos es 10
+		
+		long nL = 12345678901L;
+		
+		//Decimales
+		double nD = 123.456;
+		float nF = 123.456F;
+		
+	}
+
+}
