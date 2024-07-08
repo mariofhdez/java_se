49e514d8cdc107ba7cc4f6a3a0f00553a1a849a2
mario <mario.florez249@gmail.com>
2024-07-07 20:27:10 -0500

    Clase 16: Operadores de Asignación, Incremento y Decremento

---
+++ HolaMundo/src/IncrementDecrement.java
@@ -0,0 +1,24 @@
+
+public class IncrementDecrement {
+	
+	public static void main(String[] args) {
+		int lives = 5;
+		
+		lives = lives - 1;
+		System.out.println(lives);
+		
+		//Posfijo
+		lives--; //Decremento
+		System.out.println(lives);
+		
+		lives++; //Incremento
+		System.out.println(lives);
+		
+		//Prefijo
+		//Gana un regalo por ganar una vida
+		int gift = 100 + ++lives;
+		System.out.println(gift);
+		System.out.println(lives);
+	}
+
+}
