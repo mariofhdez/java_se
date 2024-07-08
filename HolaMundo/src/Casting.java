6ea7371a7536e8363395fe85fb58b6ba152bfe28
mario <mario.florez249@gmail.com>
2024-07-07 22:46:37 -0500

    Clase 19: Casteo entre tipo de datos

--- HolaMundo/src/Casting.java
+++ HolaMundo/src/Casting.java
@@ -17,6 +17,22 @@ public class Casting {
 		int a = 30;
 		int b = 12;
 		System.out.println((double)a/b);
+		
+		double c = a/b; //No está casteando la raíz
+		System.out.println(c);
+		
+		c = (double) a/b; //Si está casteando la raíz
+		System.out.println(c);
+		
+		//Casting de un dato menor hacia un dato mayo
+		char n = '1'; //dato menor
+		int nI = n;  //dato mayor
+		
+		System.out.println(nI);
+		
+		//Casting de un dato mayor a uno menor
+		short nS = (short) n;	//El cast debe ser explícito
+		System.out.println(nS);
 	}
 
 }
