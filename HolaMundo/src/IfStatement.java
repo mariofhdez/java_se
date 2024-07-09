626e71b960dd8822635b70b95fcc928f53885cc5
mario <mario.florez249@gmail.com>
2024-07-08 22:58:38 -0500

    Clase 23: Alcance de las variables y Sentencia ELSE

--- HolaMundo/src/IfStatement.java
+++ HolaMundo/src/IfStatement.java
@@ -2,12 +2,20 @@
 public class IfStatement {
 	public static void main(String[] args) {
 		boolean isBluetoothEnabled = true;
-		int fileSended = 3;
+		int fileSended = 3; 	//Scope global es accesible desde cualquier lugar
 		
 		if(isBluetoothEnabled) {
 			//Send file
 			fileSended++;
 			System.out.println("Archivo Enviado");
+			int i =0;	//variable declarada en scope if
+			i++;		//solo es accesible dentro del scope
+		} else {
+			fileSended--;
+			System.out.println("Por favor enciende tu Bluetooth para iniciar la transferencia");
 		}
+		
+		System.out.println(isBluetoothEnabled);
+		System.out.println(fileSended);
 	}
 }
