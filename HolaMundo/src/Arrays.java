db1d37de655d18a4f5fb621df5bb2411cdd792ef
mario <mario.florez249@gmail.com>
2024-07-09 08:21:46 -0500

    Clase 36: Declarando Arrays

---
+++ HolaMundo/src/Arrays.java
@@ -0,0 +1,26 @@
+
+public class Arrays {
+	
+	public static void main(String[] args) {
+		String [] androidVersions = new String [17];
+		String days []= new String [7];
+		
+		String [][] cities = new String[4][2];
+		
+		/*
+		 * +---------------------------+
+		 * | Country	 | City		   |
+		 * +---------------------------+
+		 * | México		 | CDMX		   |
+		 * | México		 | Guadalajara |
+		 * | Colombia	 | Bogotá	   |
+		 * | Colombia	 | Medellín	   |
+		 * +---------------------------+
+		 */
+		
+		int [][][] numbers = new int[2][2][2];
+		int [][][][] numbers4 = new int [2][2][2][2];
+		
+	}
+
+}
