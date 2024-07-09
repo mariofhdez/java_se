e6822aadf9d3f75fc40467ae4632a917d70747af
mario <mario.florez249@gmail.com>
2024-07-09 09:01:31 -0500

    Clase 38: Ciclos anidados

--- HolaMundo/src/Arrays.java
+++ HolaMundo/src/Arrays.java
@@ -1,58 +1,89 @@
 
 public class Arrays {
-	
+
 	public static void main(String[] args) {
-		String [] androidVersions = new String [17];
-		String days []= new String [7];
-		
-		String [][] cities = new String[4][2];
-		
+		String[] androidVersions = new String[17];
+		String days[] = new String[7];
+
+		String[][] cities = new String[4][2];
+
 		/*
-		 * +---------------------------+
-		 * | Country	 | City		   |
-		 * +---------------------------+
-		 * | México		 | CDMX		   |
-		 * | México		 | Guadalajara |
-		 * | Colombia	 | Bogotá	   |
-		 * | Colombia	 | Medellín	   |
-		 * +---------------------------+
+		 * +---------------------------+ | Country | City |
+		 * +---------------------------+ | México | CDMX | | México | Guadalajara | |
+		 * Colombia | Bogotá | | Colombia | Medellín | +---------------------------+
 		 */
+
+		int[][][] numbers = new int[2][2][2];
+		int[][][][] numbers4 = new int[2][2][2][2];
+
+		androidVersions[0] = "Apple Pie";
+		androidVersions[1] = "Bannana bread";
+		androidVersions[2] = "Cupcake";
+		androidVersions[3] = "Donut";
+
+		System.out.println("\n -----> Android Versions <-----");
+		//Bucle for
+/*		for (int i = 0; i < androidVersions.length; i++) {
+			System.out.println(androidVersions[i]);
+		}
+*/
 		
-		int [][][] numbers = new int[2][2][2];
-		int [][][][] numbers4 = new int [2][2][2][2];
+		//Bucle Foreach
+		for(String androidVersion: androidVersions) {
+			System.out.println(androidVersion);
+		}
+
+		/*
+		 * System.out.println(androidVersions[0]);
+		 * System.out.println(androidVersions[1]);
+		 * System.out.println(androidVersions[2]);
+		 * System.out.println(androidVersions[3]);
+		 */
+		cities[0][0] = "Colombia";
+		cities[0][1] = "Medellín";
+		cities[1][0] = "Colombia";
+		cities[1][1] = "Bogotá";
+		cities[2][0] = "México";
+		cities[2][1] = "Guadalajara";
+		cities[3][0] = "México";
+		cities[3][1] = "CDMX";
+
+		/*
+		 * System.out.println(cities [0][0]); System.out.println(cities [0][1]);
+		 * System.out.println(cities [1][0]); System.out.println(cities [1][1]);
+		 * System.out.println(cities [2][0]); System.out.println(cities [2][1]);
+		 * System.out.println(cities [3][0]); System.out.println(cities [3][1]);
+		 */
+		System.out.println("\n -----> Cities <-----");
+		//Bucle For anidado 2 dimensiones
+/*		for (int i = 0; i < cities.length; i++) {
+			for (int j = 0; j < cities[i].length; j++) {
+				System.out.println(cities[i][j]);
+			}
+		}
+*/
+		//Bucle Foreach anidado 2 dimensiones
+		for(String[] pair:cities) {
+			for(String name:pair) {
+				System.out.println(name);
+			}
+		}
 		
-		androidVersions [0] = "Apple Pie";
-		androidVersions [1] = "Bannana bread";
-		androidVersions [2] = "Cupcake";
-		androidVersions [3] = "Donut";
-		
-		System.out.println(androidVersions[0]);
-		System.out.println(androidVersions[1]);
-		System.out.println(androidVersions[2]);
-		System.out.println(androidVersions[3]);
-		
-		cities [0][0] = "Colombia";
-		cities [0][1] = "Medellín";
-		cities [1][0] = "Colombia";
-		cities [1][1] = "Bogotá";
-		cities [2][0] = "México";
-		cities [2][1] = "Guadalajara";
-		cities [3][0] = "México";
-		cities [3][1] = "CDMX";
-		
-		System.out.println(cities [0][0]);
-		System.out.println(cities [0][1]);
-		System.out.println(cities [1][0]);
-		System.out.println(cities [1][1]);
-		System.out.println(cities [2][0]);
-		System.out.println(cities [2][1]);
-		System.out.println(cities [3][0]);
-		System.out.println(cities [3][1]);
-		
-		String [][][][] animals = new String [2][3][2][2];
+		String[][][][] animals = new String[2][3][2][2];
 		animals[1][0][0][1] = "Monkey";
-		System.out.println(animals[1][0][0][1]);
-		
+//		System.out.println(animals[1][0][0][1]);
+
+		System.out.println("\n -----> Animals <-----");
+		for (int i = 0; i <= 1; i++) {
+			for (int j = 0; j <= 1; j++) {
+				for (int k = 0; k <= 1; k++) {
+					for (int l = 0; l <= 1; l++) {
+						System.out.println(animals[i][j][k][l]);
+					}
+				}
+			}
+		}
+
 	}
 
 }
