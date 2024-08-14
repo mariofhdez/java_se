020e3ec85d9b32a6dca0dc46b12f6ecd5c13261a
mario <mario.florez249@gmail.com>
2024-08-13 20:55:10 -0500

    Clase 15 Variables vs Objetos: Se realiza un ejercicio práctico para entender la diferencia entre una variable y un objeto, se evidencia como al asignar un objeto a otro objeto lo que se está asociando en realidad es el espacio en memoria.

--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -10,7 +10,28 @@ public class Main {
 		System.out.println(myDoctor.name);
 		System.out.println(myDoctor.speciallity);
 		
+		int i = 0;
+		int b = 2;
+		b = 1;
+		
+		System.out.println("\n");
+		
 		Patient patient = new Patient("Alejandra", "alejandra@email.com");
+		Patient patient2 = new Patient("Andrea", "andrea@email.com");
+		
+		System.out.println(patient.getName());
+		System.out.println(patient2.getName());
+		patient2 = patient;
+		System.out.println("\n");
+		
+		System.out.println(patient.getName());
+		System.out.println(patient2.getName());
+		
+		System.out.println("\n");
+		patient2.setName("Manuel");
+		System.out.println(patient.getName());
+		System.out.println(patient2.getName());
+		
 		patient.setWeight(54.6);
 		System.out.println(patient.getWeight());
 		
