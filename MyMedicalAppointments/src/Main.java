bd1f627f5410c6c52c6a966d6a4a7f1d776b226f
mario <mario.florez249@gmail.com>
2024-08-13 19:55:47 -0500

    Clase 12 Encapsulamiento, en este espacio se agregó una capa de seguridad a los atributos de la clase Patient para que no sean accesibles ni modificables desde cualquier lugar

--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -9,6 +9,16 @@ public class Main {
 		Doctor myDoctor = new Doctor("Anahí Salgado", "Pediatría");
 		System.out.println(myDoctor.name);
 		System.out.println(myDoctor.speciallity);
+		
+		Patient patient = new Patient("Alejandra", "alejandra@email.com");
+		System.out.println(patient.name);
+		System.out.println(patient.email);
+		
+		patient.weight = 60.5; //kg
+		patient.height = 1.65; //mt
+		
+		System.out.println(patient.weight);
+		System.out.println(patient.height);
 	}
 
 }
