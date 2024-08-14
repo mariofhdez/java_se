f92e24290d5fa744d2754cb408fb5c6e4e4c0b88
mario <mario.florez249@gmail.com>
2024-08-13 21:12:32 -0500

    Clase 16 Clases anidadas Este espacio tuvimos un acercamiento a las clases anidadas en donde para la clase Doctor se creo una clase que permitiera la gestion de las fechas disponibles para citas, para ello se creó una clase estática dentro de la misma clase doctor, se uso colecciones para agrupar las citas y se crearon getters y setters tanto para la creación de citas como para la clase estática.
    Luego en la clase main se dio un ejemplo aplicado de como crear citas, y
    como acceder a ellas a través de un foreach.

--- MyMedicalAppointments/src/Doctor.java
+++ MyMedicalAppointments/src/Doctor.java
@@ -1,30 +1,79 @@
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Doctor {
+
+	// atributos
+	static int id = 0; // autoincrement
+	private String name;
+	private String email;
+	private String speciallity;
 	
-	//atributos
-	static int id = 0; //autoincrement
-	String name;
-	String speciallity;
-	
-	//metodos
-	
-	Doctor(){
+	private ArrayList<AvailableAppointment> availableAppointments = new ArrayList<>();
+
+	// metodos
+
+	Doctor() {
 		System.out.println("Construyendo el objeto doctor");
 		id++;
 	}
-	
-	Doctor(String name, String speciallity){
+
+	Doctor(String name, String speciallity) {
 		id++;
 		this.name = name;
 		this.speciallity = speciallity;
-		System.out.println("El nombre del doctor asignado es: "+name);
+		System.out.println("El nombre del doctor asignado es: " + name);
 	}
-	
+
 	public void showName() {
 		System.out.println(name);
 	}
 
 	public void showId() {
-		System.out.println("ID Doctor: "+id);
+		System.out.println("ID Doctor: " + id);
+	}
+	
+	public void addAvailableAppointment (Date date, String time) {
+		availableAppointments.add(new AvailableAppointment(date, time));
+	}
+	
+	public ArrayList<AvailableAppointment> getAvailableAppointments(){
+		return availableAppointments;
+	}
+
+	public static class AvailableAppointment {
+		private int id_availableAppointment;
+		private Date date;
+		private String time;
+
+		public int getId_availableAppointment() {
+			return id_availableAppointment;
+		}
+
+		public void setId_availableAppointment(int id_availableAppointment) {
+			this.id_availableAppointment = id_availableAppointment;
+		}
+
+		public Date getDate() {
+			return date;
+		}
+
+		public void setDate(Date date) {
+			this.date = date;
+		}
+
+		public String getTime() {
+			return time;
+		}
+
+		public void setTime(String time) {
+			this.time = time;
+		}
+
+		public AvailableAppointment(Date date, String time) {
+			this.date = date;
+			this.time = time;
+		}
+
 	}
 }
--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -1,5 +1,7 @@
 import static ui.UIMenu.*;
 
+import java.util.Date;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -7,36 +9,39 @@ public class Main {
 		//showMenu();
 		
 		Doctor myDoctor = new Doctor("Anahí Salgado", "Pediatría");
-		System.out.println(myDoctor.name);
-		System.out.println(myDoctor.speciallity);
 		
-		int i = 0;
-		int b = 2;
-		b = 1;
+		myDoctor.addAvailableAppointment(new Date(), "4pm");
+		myDoctor.addAvailableAppointment(new Date(), "10am");
+		myDoctor.addAvailableAppointment(new Date(), "1pm");
 		
-		System.out.println("\n");
+		for(Doctor.AvailableAppointment avaApp: myDoctor.getAvailableAppointments()) {
+			System.out.println(avaApp.getDate() + "  "+ avaApp.getTime());
+		}
 		
-		Patient patient = new Patient("Alejandra", "alejandra@email.com");
-		Patient patient2 = new Patient("Andrea", "andrea@email.com");
 		
-		System.out.println(patient.getName());
-		System.out.println(patient2.getName());
-		patient2 = patient;
-		System.out.println("\n");
-		
-		System.out.println(patient.getName());
-		System.out.println(patient2.getName());
-		
-		System.out.println("\n");
-		patient2.setName("Manuel");
-		System.out.println(patient.getName());
-		System.out.println(patient2.getName());
-		
-		patient.setWeight(54.6);
-		System.out.println(patient.getWeight());
-		
-		patient.setPhoneNumber("123454678");
-		System.out.println(patient.getPhoneNumber());
+//		System.out.println("\n");
+//		
+//		Patient patient = new Patient("Alejandra", "alejandra@email.com");
+//		Patient patient2 = new Patient("Andrea", "andrea@email.com");
+//		
+//		System.out.println(patient.getName());
+//		System.out.println(patient2.getName());
+//		patient2 = patient;
+//		System.out.println("\n");
+//		
+//		System.out.println(patient.getName());
+//		System.out.println(patient2.getName());
+//		
+//		System.out.println("\n");
+//		patient2.setName("Manuel");
+//		System.out.println(patient.getName());
+//		System.out.println(patient2.getName());
+//		
+//		patient.setWeight(54.6);
+//		System.out.println(patient.getWeight());
+//		
+//		patient.setPhoneNumber("123454678");
+//		System.out.println(patient.getPhoneNumber());
 	}
 
 }
